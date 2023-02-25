0095b0228e3a89a7d8d5220f4343954c675f18f2
Lefju <93533854+Lefjuu@users.noreply.github.com>
2023-02-25 23:51:13 +0100

    fix: typo

--- src/lib/redis.lib.js
+++ src/lib/redis.lib.js
@@ -7,7 +7,7 @@ const connect = () =>
     new Promise((resolve, reject) => {
         const r = new Redis(
             "rediss://red-cft8apqrrk0c8352qoag:p7UzzKXXXPWkfKZumYuN74m2ieQFHrc6@frankfurt-redis.render.com:6379"
-
+)
         r.on('connect', function () {
             console.log('✅ Redis: connected!')
             redis = r
