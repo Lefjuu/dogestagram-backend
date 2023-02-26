c132f1572754465df58f53eb5cc884bd9a2e26fa
Lefju <karol.legut121@gmail.com>
2023-02-26 10:20:40 +0100

    marge conflict

--- src/lib/redis.lib.js
+++ src/lib/redis.lib.js
@@ -6,7 +6,6 @@ let redis
 const connect = () =>
     new Promise((resolve, reject) => {
         const r = new Redis(
-<<<<<<< HEAD
             // REDIS_PASSWORD
             // ? `redis://:${REDIS_PASSWORD}@${REDIS_HOSTNAME}${REDIS_PORT}/0`
             // : `redis://${REDIS_HOSTNAME}:${REDIS_PORT}`
@@ -14,10 +13,6 @@ const connect = () =>
         )
         // console.log(`redis://${REDIS_HOSTNAME}:${REDIS_PORT}`)
 
-=======
-            "rediss://red-cft8apqrrk0c8352qoag:p7UzzKXXXPWkfKZumYuN74m2ieQFHrc6@frankfurt-redis.render.com:6379"
-)
->>>>>>> 0095b0228e3a89a7d8d5220f4343954c675f18f2
         r.on('connect', function () {
             console.log('✅ Redis: connected!')
             redis = r
