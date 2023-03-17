966c34afd74e6bc77b06f2eff7a43afec9fe5a62
Lefju <karol.legut121@gmail.com>
2023-03-17 19:11:41 +0100

    fix: hosting bug

--- src/api/routers/auth.routes.js
+++ src/api/routers/auth.routes.js
@@ -4,8 +4,8 @@ import { mw } from '../../services/mw.service.js'
 export default (app) => {
     app.post('/api/auth/login', AuthController.login)
     app.post('/api/auth/register', AuthController.register)
-    app.post('/api/auth/recover', AuthController.recover)
     app.post('/api/auth/forgot-password', AuthController.sendEmail)
+    app.post('/api/auth/recover', AuthController.recover)
     app.patch('/api/auth/new-password/:string', AuthController.setNewPassword)
     app.get('/api/auth/me', mw(['user']), AuthController.me)
 }
