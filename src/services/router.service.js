afee57dbe5a1f907393cf407b55dc06b4f1599cb
Lefju <karol.legut121@gmail.com>
2023-10-29 22:02:27 +0100

    patch: async routes loading

--- src/services/router.service.js
+++ src/services/router.service.js
@@ -7,15 +7,14 @@ exports.routes = async app => {
     const routerPath = `${BASE_PATH}/api/routers`;
     const files = fs.readdirSync(routerPath);
 
-    files.forEach(route => {
+    files.forEach(async route => {
         app.use(globalErrorHandler);
 
         const routeModule = require(`${routerPath}/${route}`);
-
         if (typeof routeModule === 'function') {
-            routeModule(app);
+            await routeModule(app);
         } else if (typeof routeModule.default === 'function') {
-            routeModule.default(app);
+            await routeModule.default(app);
         } else {
             console.error(`Invalid module export for route: ${route}`);
         }
